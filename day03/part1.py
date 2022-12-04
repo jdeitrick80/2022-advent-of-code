380012c006feefedd1166d252779ba76a9c3a104
jdeitrick80 <jed.deitrick@gmail.com>
2022-12-04 10:42:22 +0000

    day03

--- day03/part1.py
+++ day03/part1.py
@@ -1,2 +1,3 @@
+def common(s): return ''.join(set(s[:int(len(s)/2)]).intersection(s[int(len(s)/2):]))
 def val(c): return ord(c)-38 if c.isupper() else ord(c)-96
-with open('input.txt') as f: print(sum(map(lambda l: val(''.join(set(l[:int(len(l)/2)]).intersection(l[int(len(l)/2):]))), f.read().split('\n'))))
+with open('input.txt') as f: print(sum(map(lambda l: val(common(l)), f.read().split('\n'))))
--- day03/part2.py
+++ day03/part2.py
@@ -1,3 +1,4 @@
 import re
+def common(s): return ''.join(set(''.join(set(s[0]).intersection(s[1]))).intersection(s[2])) 
 def val(c): return ord(c)-38 if c.isupper() else ord(c)-96
-with open('input.txt') as f: print(sum(map(lambda k: val(''.join(set(''.join(set(k[0]).intersection(k[1]))).intersection(k[2]))), map(lambda l: l.split('\n'),re.findall('((?:[^\n]+\n?){1,3})', f.read())))))
+with open('input.txt') as f: print(sum(map(lambda k: val(common(k)), map(lambda l: l.split('\n'),re.findall('((?:[^\n]+\n?){1,3})', f.read())))))
