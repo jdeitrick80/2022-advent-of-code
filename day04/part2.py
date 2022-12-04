96bfdaedd2c340fe96ef85c59dc3733b9eddb22a
jdeitrick80 <jed.deitrick@gmail.com>
2022-12-04 09:02:06 +0000

    day04

---
+++ day04/part2.py
@@ -0,0 +1,4 @@
+import re
+def checkOverlap(i,j,x,y):
+    return int(((i>=x and i<=y) or (j>=x and j<=y)) or ((x>=i and x<=j) or (y>=i and y<=j)))
+with open('input.txt') as f: print(sum(map(lambda line: checkOverlap(*tuple(map(int,re.split(r"[,-]",line)))),f.read().split('\n'))))
