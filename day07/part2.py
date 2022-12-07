727174b5e0e9eaf0e7362efd2c040de0bc63ec83
jdeitrick80 <jed.deitrick@gmail.com>
2022-12-07 19:35:10 +0000

    day07

---
+++ day07/part2.py
@@ -0,0 +1,28 @@
+with open('input.txt') as f:
+  lines = f.read().split('\n')
+pwd = []
+directories = {}
+for line in lines:
+    line = line.split(' ')
+    if(line[0]=='$'):
+        if(line[1]=='cd'):
+            if(line[2]=='..'): pwd = pwd[:-1]
+            else: pwd.append(line[2])
+    else:
+        try:
+            size = int(line[0])
+        except:
+            size=0
+        for i in range(len(pwd)):
+            p=''
+            if(i==0): p = '/'.join(pwd)
+            else: p = '/'.join(pwd[:-i])
+            if p in directories: directories[p]+=size
+            else: directories[p]=size
+
+need = 30000000-(70000000-directories['/'])
+smallest=70000000
+for k in directories:
+    if(directories[k]>=need and directories[k]<smallest):
+        smallest=directories[k]
+print(smallest)
